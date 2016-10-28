b6fc3dfa9587b717e3127423666b0334dd159275
Omar Wilson <omar.wilson3@comcast.net>
2016-10-28 14:52:40 -0700

    Just some basic button setup

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Omar Wilson on 10/28/16.
+//  Copyright © 2016 Omar Wilson. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}

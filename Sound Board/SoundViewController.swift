9d604c9691c634b09fd0f373900c5ef806565557
Omar Wilson <omar.wilson3@comcast.net>
2016-10-30 14:27:31 -0700

    Started setting up audio sessions. This is not working yet. I still need to make try-catch and other setup

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
 
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     
+    var audioRecorder : AVAudioRecorder?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpRecorder()
     }
 
+    func setUpRecorder() {
+        // Create an adio session
+        let session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        session.overrideOutputAudioPort(.speaker)
+        session.setActive(true)
+        
+        // Create URL for the file
+        
+        // Create settings for the audio recorder
+        
+        
+        // set up AudioRecorder object
+        audioRecorder = AVAudioRecorder(url: <#T##URL#>, settings: <#T##[String : Any]#>)
+    }
 
     @IBAction func recordTapped(_ sender: Any) {
     }
